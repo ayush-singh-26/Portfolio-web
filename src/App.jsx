324c41e5cd07f8075bedceff0976698373d7edd2
ayush-singh-225 <ayushsinghtemp@gmail.com>
2024-12-01 18:37:48 +0530

    bug fix 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Banner from './components/Banner/Banner'
 import Contact from './components/Contact/Contact'
-import Footer from './components/Folder/Footer'
+import Footer from './components/Footer/Footer'
 import Navbar from './components/Navbar'
 import Projects from './components/Projects/Projects'
 import Skill from './components/Skill/Skill'
